72977b30b46fa18389381a9642c955eb611e2b1c
Simon Ser <contact@emersion.fr>
2021-03-03 09:56:23 +0100

    Log errors to console on command failure

--- components/app.js
+++ components/app.js
@@ -781,6 +781,7 @@ export default class App extends Component {
 		try {
 			cmd(this, args);
 		} catch (error) {
+			console.error("Failed to execute command '" + name + "'", error);
 			this.setState({ error });
 		}
 	}
