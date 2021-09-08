Libera-Chat/gamja

commit a51be5037d3f32ad09e04bb6c2d0eb745d2fadaa

lib/irc: only add colon to trailing arg when necessary
This mirrors go-irc's behavior:

https://github.com/go-irc/irc/blob/7ba1a1858f5ee2a44f18501b486ec11dd1990018/parser.go#L374

Closes: https://todo.sr.ht/~emersion/gamja/103

lib/irc.js
@@ -215,11 +215,14 @@ export function formatMessage(msg) {
 	}
 	s += msg.command;
 	if (msg.params && msg.params.length > 0) {
-		let last = msg.params[msg.params.length - 1];
-		if (msg.params.length > 1) {
-			s += " " + msg.params.slice(0, -1).join(" ");
+		s += " " + msg.params.slice(0, -1).join(" ");
+
+		let last = String(msg.params[msg.params.length - 1]);
+		if (last.length === 0 || last === ":" || last.indexOf(" ") >= 0) {
+			s += " :" + last;
+		} else {
+			s += " " + last;
 		}
-		s += " :" + last;
 	}
 	s += "\r\n";
 	return s;

commit f17d61ed86d349a5d4bce90a64f1c416814be30f

lib/irc: fix formatMessage when last arg starts with colon
Fixes: a51be5037d3f ("lib/irc: only add colon to trailing arg when necessary")

lib/irc.js
@@ -218,7 +218,7 @@ export function formatMessage(msg) {
 		s += " " + msg.params.slice(0, -1).join(" ");
 
 		let last = String(msg.params[msg.params.length - 1]);
-		if (last.length === 0 || last === ":" || last.indexOf(" ") >= 0) {
+		if (last.length === 0 || last.startsWith(":") || last.indexOf(" ") >= 0) {
 			s += " :" + last;
 		} else {
 			s += " " + last;