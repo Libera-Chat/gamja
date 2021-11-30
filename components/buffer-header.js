Libera-Chat/gamja

commit 86853eb2e5521565621efd193481c6a36639ee14

components/buffer-header: hide action buttons when disconnected

components/buffer-header.js
@@ -38,7 +38,12 @@ export default function BufferHeader(props) {
 		props.onManageNetwork();
 	}
 
-	let description = null, actions = null;
+	let fullyConnected = props.server.status === ServerStatus.REGISTERED;
+	if (props.bouncerNetwork) {
+		fullyConnected = fullyConnected && props.bouncerNetwork.state === "connected";
+	}
+
+	let description = null, actions = [];
 	switch (props.buffer.type) {
 	case BufferType.SERVER:
 		switch (props.server.status) {
@@ -74,43 +79,52 @@ export default function BufferHeader(props) {
 			break;
 		}
 
+		let joinButton = html`
+			<button
+				key="join"
+				onClick=${handleJoinClick}
+			>Join channel</button>
+		`;
+
 		if (props.isBouncer) {
 			if (props.server.isupport.get("BOUNCER_NETID")) {
-				actions = html`
-					<button
-						key="join"
-						onClick=${handleJoinClick}
-					>Join channel</button>
-					<button
-						key="manage"
-						onClick=${handleManageNetworkClick}
-					>Manage network</button>
-				`;
+				if (fullyConnected) {
+					actions.push(joinButton);
+					actions.push(html`
+						<button
+							key="manage"
+							onClick=${handleManageNetworkClick}
+						>Manage network</button>
+					`);
+				}
 			} else {
-				actions = html`
-					<button
-						key="add"
-						onClick=${handleAddNetworkClick}
-					>Add network</button>
+				if (fullyConnected) {
+					actions.push(html`
+						<button
+							key="add"
+							onClick=${handleAddNetworkClick}
+						>Add network</button>
+					`);
+				}
+				actions.push(html`
 					<button
 						key="disconnect"
 						class="danger"
 						onClick=${handleCloseClick}
 					>Disconnect</button>
-				`;
+				`);
 			}
 		} else {
-			actions = html`
-				<button
-					key="join"
-					onClick=${handleJoinClick}
-				>Join channel</button>
+			if (fullyConnected) {
+				actions.push(joinButton);
+			}
+			actions.push(html`
 				<button
 					key="disconnect"
 					class="danger"
 					onClick=${handleCloseClick}
 				>Disconnect</button>
-			`;
+			`);
 		}
 		break;
 	case BufferType.CHANNEL:
@@ -126,11 +140,15 @@ export default function BufferHeader(props) {
 				>Leave</button>
 			`;
 		} else {
+			if (fullyConnected) {
+				actions.push(html`
+					<button
+						key="join"
+						onClick=${handleJoinClick}
+					>Join</button>
+				`);
+			}
 			actions = html`
-				<button
-					key="join"
-					onClick=${handleJoinClick}
-				>Join</button>
 				<button
 					key="part"
 					class="danger"

commit 1e84412172ef52d04e88aa7d746a9112576e12e9

Show "Manage network" even if upstream is disconnected
Fixes: 86853eb2e552 ("components/buffer-header: hide action buttons when disconnected")

components/buffer-header.js
@@ -90,6 +90,8 @@ export default function BufferHeader(props) {
 			if (props.server.isupport.get("BOUNCER_NETID")) {
 				if (fullyConnected) {
 					actions.push(joinButton);
+				}
+				if (props.server.status === ServerStatus.REGISTERED) {
 					actions.push(html`
 						<button
 							key="manage"