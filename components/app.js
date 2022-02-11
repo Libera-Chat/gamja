Libera-Chat/gamja

commit c428e504fe9c369f3d9a44e9127350070e657bab

Don't show unread marker for outgoing messages
Closes: https://todo.sr.ht/~emersion/gamja/100

components/app.js
@@ -469,17 +469,25 @@ export default class App extends Component {
 		this.setBufferState(bufID, (buf) => {
 			// TODO: set unread if scrolled up
 			let unread = buf.unread;
+			let prevReadReceipt = buf.prevReadReceipt;
+
 			if (this.state.activeBuffer !== buf.id) {
 				unread = Unread.union(unread, msgUnread);
 			} else {
 				this.setReceipt(bufName, ReceiptType.READ, msg);
 			}
+
+			// Don't show unread marker for my own messages
+			if (client.isMyNick(msg.prefix.name)) {
+				prevReadReceipt = { time: msg.tags.time };
+			}
+
 			this.bufferStore.put({
 				name: buf.name,
 				server: client.params,
 				unread,
 			});
-			return { unread };
+			return { unread, prevReadReceipt };
 		});
 	}
 

commit 39c36e7a7bdb4bf2f3bb7d09ee1d0499277a9bed

Fix unread marker going back
Receipts must never go back in time.

Fixes: c428e504fe9c ("Don't show unread marker for outgoing messages")

components/app.js
@@ -586,7 +586,7 @@ export default class App extends Component {
 			}
 
 			// Don't show unread marker for my own messages
-			if (client.isMyNick(msg.prefix.name)) {
+			if (client.isMyNick(msg.prefix.name) && (!prevReadReceipt || prevReadReceipt.time < msg.tags.time)) {
 				prevReadReceipt = { time: msg.tags.time };
 			}
 